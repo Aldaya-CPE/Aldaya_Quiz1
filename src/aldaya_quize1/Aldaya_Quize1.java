271fcaa9182ff72f3ea4383e4ea31025c1cbbba9
Student <Student@192.168.3.90>
2023-09-04 16:21:13 +0800

    Version1

---
+++ src/aldaya_quize1/Aldaya_Quize1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aldaya_quize1;
+
+/**
+ *
+ * @author Student
+ */
+public class Aldaya_Quize1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       
+    }
+    
+}
